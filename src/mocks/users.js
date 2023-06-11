6debc6dc0b5397b91fa8d52214651a96cc8c08f6
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-06-11 20:22:28 -0300

    feat: mock for user controller

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Ana'
+  },
+  {
+    id: 2,
+    name: 'Vivian'
+  },
+  {
+    id: 3,
+    name: 'Aline'
+  },
+  {
+    id: 4,
+    name: 'Juliana'
+  }
+];
